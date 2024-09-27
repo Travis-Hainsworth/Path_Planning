188b2eaeddce1ce6f432fe49012fb165b1c3e727
Travis Hainsworth <33101501+Travis-Hainsworth@users.noreply.github.com>
2024-09-27 11:58:40 -0600

    Add files via upload

---
+++ helper_functions/screen_helper_functions.py
@@ -0,0 +1,88 @@
+# Screen Helper Functions
+#
+# Helper functions for displaying information on the screen
+#   make_screen - initialize the pygame screen
+#   draw_path - highlights the path on the screen in green
+#   draw_cost_values - display the value/cost of each node
+#
+# Author:
+#   Travis Hainsworth
+#   CSCI 3022 - Introduction to Robotics
+#   CU Boulder
+#   Fall 2024
+import pygame
+
+
+def make_screen(world, screen_size):
+    pygame.init()
+    screen = pygame.display.set_mode(screen_size)
+    screen.fill((255, 255, 255))
+
+    pygame.draw.rect(screen, (0, 255, 0), (world.start[1]*world.pixel_width, world.start[0]*world.pixel_height, world.pixel_width, world.pixel_height))
+    pygame.draw.rect(screen, (255, 0, 0), (world.goal[1]*world.pixel_width, world.goal[0]*world.pixel_height, world.pixel_width, world.pixel_height))
+    pygame.display.flip()
+
+    return screen
+
+
+def draw_path(world, screen):
+    goal_pos = world.goal
+    node = world.grid[goal_pos[0]][goal_pos[1]]   
+    while node.previous:        
+        node = node.previous
+        node_row = node.position[0]
+        node_col = node.position[1]
+        
+        pygame.draw.rect(screen, (0,255,0), (node_col*world.pixel_width, node_row*world.pixel_height, world.pixel_width, world.pixel_height))
+        pygame.display.flip()
+
+
+
+def draw_cost_values(world, screen):
+    
+    font = pygame.font.Font(None, 20)
+
+    list_o_nodes = [node for row in world.grid for node in row]
+    for node in list_o_nodes:
+        node_row = node.position[0]
+        node_col = node.position[1]
+
+        cost = node.true_cost
+        text = font.render(str(cost), True, (0,0,0))
+
+        rectangle = pygame.Rect(node_col*world.pixel_width, node_row*world.pixel_height, world.pixel_width, world.pixel_height)
+
+        text_rect = text.get_rect(center=rectangle.center)
+
+
+        screen.blit(text, text_rect)
+
+    pygame.display.flip()  
+
+
+def color_explore(world, screen):
+    gray_color = (100, 100, 100)
+
+    list_o_nodes = [node for row in world.grid for node in row]
+
+    for node in list_o_nodes:
+        if not node.wall and node.true_cost == float('inf'):
+            node_row = node.position[0]
+            node_col = node.position[1]
+
+            pygame.draw.rect(screen, gray_color, (node_col*world.pixel_width, node_row*world.pixel_height, world.pixel_width, world.pixel_height))
+    
+    pygame.display.flip()
+
+
+def update_RRT_graph(new_node, closest_node, world, screen):
+    color = (40, 40, 40)
+    # Draw a dot where the new node is:
+    new_position = (new_node.position[1] * world.pixel_height, new_node.position[0] * world.pixel_width)
+    pygame.draw.circle(screen, color, new_position, 2)
+
+    # Draw a line between the new node and the closest node
+    closest_position = (closest_node.position[1] * world.pixel_height, closest_node.position[0] * world.pixel_width)
+    pygame.draw.line(screen, color, closest_position, new_position)
+
+    pygame.display.flip()
---
+++ helper_functions/world_class_definition.py
@@ -0,0 +1,87 @@
+# Definition of a class representing the world
+#
+# This object contains all of the nodes in the path planning graph
+#
+# The world class has the following info:
+#   grid        - a 2d list of the nodes arranged by [row][column]
+#   start       - a tuple of the starting (row, column)
+#   goal        - a tuple of the goal (row, column)
+#   action_cost - an int representing how much it costs to take an action
+#
+# There is also info that you shouldn't need to use or worry about:
+#   pixel dimensions - calculated based on the world's grid size and the size of the screen
+#
+#   There are also two function:
+#       compile_neighbors - adds the neighbors of each node to the node's neighbors list
+#       add_wall - modifies the node's wall boolean and also updates the pygame screen
+#
+# Author:
+#   Travis Hainsworth
+#   CSCI 3022 - Introduction to Robotics
+#   CU Boulder
+#   Fall 2024
+import pygame
+from helper_functions.node_class_definition import *
+
+class world_class:
+    def __init__(self, world_height, world_width, start_location, goal_location, screen_size):
+        # Create the list of lists that holds the nodes
+        self.grid = [[node_class(i,j) for j in range(world_width)] for i in range(world_height)]
+
+        # Save the start and goal locations, as well as how much it costs to take an action
+        self.start = start_location
+        self.goal = goal_location
+        self.action_cost = 1
+
+        # Define the start's cost as zero
+        self.grid[self.start[0]][self.start[1]].true_cost = 0
+
+        # This flattened_grid is convenient to have if you are going to be making dictionaries 
+        # in the future
+        self.flattened_grid = [node for row in self.grid for node in row]
+
+        # Save the world and pixel dimensions
+        self.world_height = world_height
+        self.world_width = world_width
+        self.world_size = (world_height, world_width)
+
+
+        self.pixel_height = screen_size[0]//world_height
+        self.pixel_width = screen_size[1]//world_width
+
+    # Here we will fill in the node's list of neighbors based on the neighbors touching each node
+    def compile_neighbors(self):
+        for row in self.grid:
+            for node in row:
+                node_row, node_col = node.position
+
+                # Up action
+                if node_row != 0:
+                    neighbor = self.grid[node_row-1][node_col]
+                    if not neighbor.wall:
+                        node.neighbors.append(neighbor)
+                # Down action
+                if node_row != self.world_height-1:
+                    neighbor = self.grid[node_row+1][node_col]
+                    if not neighbor.wall:
+                        node.neighbors.append(neighbor)
+                # Right action
+                if node_col != 0:
+                    neighbor = self.grid[node_row][node_col-1]
+                    if not neighbor.wall:
+                        node.neighbors.append(neighbor)
+                # Left action
+                if node_col != self.world_width-1:
+                    neighbor = self.grid[node_row][node_col+1]
+                    if not neighbor.wall:
+                        node.neighbors.append(neighbor)
+                
+    # This is used for dynamically adding walls to the world in the interactive map
+    def add_wall(self, screen):
+        wall_color = (0, 0, 0)
+        mouse_location = pygame.mouse.get_pos()
+        node_col = mouse_location[0]//self.pixel_height
+        node_row = mouse_location[1]//self.pixel_width
+        pygame.draw.rect(screen, wall_color, (node_col*self.pixel_width, node_row*self.pixel_height, self.pixel_width, self.pixel_height))
+        pygame.display.flip()
+        self.grid[node_row][node_col].wall = True
